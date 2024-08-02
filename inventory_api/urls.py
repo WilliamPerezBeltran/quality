5335cadc0c4df13bba8518cb46d3eda51787e0c1
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-08-02 14:19:03 -0500

    add configuration rest, logger and fix urls

--- inventory_api/urls.py
+++ inventory_api/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path
-from .views import ProductAPIView, InventoryAPIView, SaleAPIView, SalesReportAPIView
+from inventory.views import ProductAPIView, InventoryAPIView, SaleAPIView, SalesReportAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
