3161c13e0b2399eda4da7296574980aa681baa4e
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-08-02 13:46:43 -0500

    add models product, inventory, sale and initial migration

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+# inventory/models.py
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Inventory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    warehouse = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.warehouse}"
+
+class Sale(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    sale_date = models.DateTimeField(auto_now_add=True)
+    warehouse = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Sale {self.id} - {self.product.name}"
